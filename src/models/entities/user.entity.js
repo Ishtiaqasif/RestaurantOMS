153868269ee4d3e00bf316dcb8e75069f6a6a866
Asif Ishtiaq <ishtiaqasif03@gmail.com>
2022-03-19 01:50:22 +0600

    New db connected, User insert api implemented

--- src/models/entities/user.entity.js
+++ src/models/entities/user.entity.js
@@ -1,76 +1,34 @@
-const Joi = require('joi');
-const { Schema } = require('mongoose');
-const BaseEntity = require('./bases/base.entity');
+const Joi = require("joi");
+const { Schema, Mongoose } = require("mongoose");
+const { UserSchema } = require("../schemas/user.schema");
+const BaseEntity = require("./bases/base.entity");
 
-class User
-{
-    static ContactSchema = new Schema({  // contact array cannot be limited
-        contact: {
-           type: String,
-           max: 15,
-           //match: /(^(+88|0088)?(01){1}[3456789]{1}(\d){8})$/
-        }
-    });
-
-    static getDbSchema(){
-        let dbSchema =  {
-            email: { 
+class User {
+    static getDbSchema() {
+        let dbSchema = {
+            email: {
                 type: String,
-                required: [true, 'Email is required'],
+                required: [true, "Email is required"],
                 trim: true,
                 lowercase: true,
-                match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
-                validate: {
-                    isAsync: true,
-                    validator: (value, isValid) => {
-                        return this.constructor.findOne({ user: value })
-                        .exec((err, user)=>{
-                            
-                            if(err) throw err;
-                            
-                            if(user) {
-                                if(this.email === user.email) return isValid(true);
-                                return isValid(false);  
-                            }
-                            else return isValid(true);
-        
-                        });
-                    },
-                    message:  'This email is already taken!'
-                },
+                match: [
+                    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                    "Please fill a valid email address",
+                ]
             },
 
-            username:{  //unique:true 
+            username: {
+                //unique:true
                 type: String,
                 uppercase: true,
                 trim: true,
-                maxLength: [100, 'Must not be more than 100, got {VALUE}'],
-                required: [true, 'Username is required'],
-                validate: {
-                    isAsync: true,
-                    validator: (value, isValid) => {
-                        return this.constructor.findOne({ username: value })
-                        .exec((err, user)=>{
-                            
-                            if(err) throw err;
-                            
-                            return user?
-                                //if(this.username === user.username) return isValid(true);
-                                //return 
-                                isValid(false):
-                            //}
-                            //else return 
-                            isValid(true);
-        
-                        });
-                    },
-                    message:  'This Username is already taken!'
-                },
+                maxLength: [100, "Must not be more than 100, got {VALUE}"],
+                required: [true, "Username is required"],
             },
 
             address: {
                 type: String,
-                maxLength: 250
+                maxLength: 250,
             },
 
             bloodGroup: {
@@ -81,42 +39,49 @@ class User
 
             dateOfBirth: {
                 type: Date,
-                min: '1950-01-01',
-                max: new Date(new Date().setFullYear(new Date().getFullYear() - 10)).toISOString().split('T')[0]
+                min: "1950-01-01",
+                max: new Date(
+                    new Date().setFullYear(new Date().getFullYear() - 10)
+                )
+                    .toISOString()
+                    .split("T")[0],
             },
 
-            contacts: {
-                type: [this.ContactSchema]
-            },
-
+            contacts: [
+                {
+                    type: String,
+                    max: 15,
+                    //match: /(^(+88|0088)?(01){1}[3456789]{1}(\d){8})$/,
+                },
+            ],
             fullName: {
                 type: String,
                 trim: true,
                 maxLength: 100,
-                required: [true, 'Name is required']
+                required: [true, "Name is required"],
             },
 
             gender: {
                 type: String,
-                enum: { 
-                    values: ['Male', 'Female', 'Undisclosed'], 
-                    message: '{VALUE} is not supported' 
+                enum: {
+                    values: ["Male", "Female", "Undisclosed"],
+                    message: "{VALUE} is not supported",
                 },
-                default: 'Undisclosed',
+                default: "Undisclosed",
             },
 
-            password: { //////
-                type: String,  //hash
+            password: {
+                type: String, //hash
                 maxLength: 100,
                 trim: true,
-                required: [true, 'Password is required']
+                required: [true, "Password is required"],
             },
 
-            adminLevel: { /////
+            adminLevel: {
                 type: Array, //enum
                 maxLength: 100,
-                default: '1',
-                required: [true, 'Admin Role is required']
+                default: "1",
+                required: [true, "Admin Role is required"],
             },
 
             isActive: {
@@ -131,21 +96,20 @@ class User
         };
 
         return dbSchema;
-
-    } 
-    static getValidationSchema(){
+    }
+    static getValidationSchema() {
         return {
             name: Joi.string().required(),
-            description: Joi.string().required()
-        }
+            description: Joi.string().required(),
+        };
     }
-    static getInitializedObject(){ 
+    static getInitializedObject() {
         return {
             name: "",
             description: "",
             metadata: BaseEntity.getInitializedBaseObject(),
-        }
-    } 
+        };
+    }
 }
 
 module.exports = User;
--- src/models/schemas/user.schema.js
+++ src/models/schemas/user.schema.js
@@ -3,14 +3,15 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const UserEntity = require("../entities/user.entity");
 
-const UserSchema = new Schema(UserEntity.getDbSchema(), 
-{
+const writeConcern = {
     writeConcern: {
         w: "majority",
         j: true,
         wtimeout: 1000,
     },
-});
+};
+
+const UserSchema = new Schema(UserEntity.getDbSchema(), writeConcern);
 
 //const UserValidationSchema = Joi.object(UserEntity.getValidationSchema());
 
